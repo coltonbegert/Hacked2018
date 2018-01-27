10e5024b1fb0dfe9a421f056fbf9dc979f5db0f9
Colton Begert <begert@ualberta.ca>
2018-01-27 13:09:03 -0700

    added serial

--- passman/passman.ino
+++ passman/passman.ino
@@ -1,9 +1,22 @@
+#include <SD.h>
+#include <Adafruit_NeoPixel.h>
+
+#ifdef __AVR__
+#include <avr/power.h>
+#endif
+
+#define PIN 6
+
+Adafruit_NeoPixel strip = Adafruit_NeoPixel(32, PIN, NEO_GRB + NEO_KHZ800);
+
 void setup() {
   // put your setup code here, to run once:
+  Serial.begin(9600);
 
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
-
+  Serial.println("test");
+  delay(1000);
 }
