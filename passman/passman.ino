36fdc49eb6a899e053ebe0780e7b2f0011419c6b
Colton Begert <begert@ualberta.ca>
2018-01-27 19:52:06 -0700

    message passing between arduino and python now works!

--- passman/passman.ino
+++ passman/passman.ino
@@ -9,10 +9,14 @@
 
 Adafruit_NeoPixel strip = Adafruit_NeoPixel(32, PIN, NEO_GRB + NEO_KHZ800);
 
-
-struct  Message{
-  char type;
+union byte_union{
   uint16_t length;
+  uint8_t bytes[2];
+};
+
+struct Message{
+  char type;
+  union byte_union length;
   char *message;
 };
 
@@ -21,31 +25,54 @@ void send_message(struct Message *m) {
 
   Serial.print(m->type);
 
-  Serial.write((uint8_t *) &m->length, 2);
-  Serial.print(m->message);
-  // for (int i = 0; i < message.length; i++) {
-  //   Serial.print(m.message[i])
-  // }
+  Serial.write((uint8_t *) &m->length.length, 2);
+  Serial.write((uint8_t *)m->message, m->length.length);
+
 }
 
 void recv_message(struct Message *m) {
+  // Serial.print("recv");
+  // int incomingByte;
+  // if (Serial.available() > 0)
+  // {
+    //   // read the incoming byte:
+    // incomingByte = Serial.read();
+
+    //   // say what you got:
+    //   // Serial.print("I received: ");
+    // Serial.write(incomingByte);
+    // Serial.print(Serial.available());
+  // }
+  // return;
   // struct Message m;
-  // Serial.read((uint8_t *)&m->type, 1);
+  // Serial.read((uint8_t *)&m->type, 1);¨
   int len = 4;
   // Serial.print("hello");
   // delay(100);
   m->type = (char)Serial.read();
   // // Serial.print(m->type);
-  char byte1,byte2;
+  uint8_t byte1,byte2,byte0;
+  // byte0 = Serial.read();
+  byte0 = Serial.read();
   byte1 = Serial.read();
-  byte2 = Serial.read();
-  uint16_t sho = (byte1 << 8) + byte2;
+
+  m->length.bytes[0] = byte1;
+  m->length.bytes[1] = byte0;
+
+  // uint16_t sho = ((uint16_t)byte1 << 8) | byte2;
   // m->length = sho;
-  // m->length = (byte1 << 8) | byte2;
-  while (Serial.available() > 0) {
-    Serial.read();
-    delay(100);
+  // m->length = ((uint16_t)byte1 << 8) | byte2;
+  // m->length = ((uint16_t)Serial.read() << 8) | Serial.read();
+  m->message = malloc(m->length.length * sizeof(char));
+  for (int i = 0; i < m->length.length; i++) {
+    m->message[i] = (char)Serial.read();
+    // Serial.write((uint8_t *)&m->message[i], 1);
   }
+  // while (Serial.available() > 0)
+  // {
+  //   Serial.read();
+  //   delay(100);
+  // }
   // // m->length[0] = (void)Serial.read();
   // // m->length[1] = (void)Serial.read();
   // // Serial.print(m->type);
@@ -56,7 +83,27 @@ void recv_message(struct Message *m) {
   //   m->message[i] = Serial.read();
   // }
 
-  Serial.print("world");
+  // Serial.print("world");
+
+
+
+  // Serial.print(byte0);
+  // Serial.print(byte1);
+  // Serial.print(byte2);
+
+  //  m->type = (char) byte0;
+  // m->type = (char) byte0;
+
+  // Serial.print(m->type);
+  // Serial.print("a");
+  // Serial.print(m->length.length);
+  // Serial.print("b");
+  // Serial.print(m->length.byt[0]);
+  // Serial.print("c");
+  // Serial.print(m->length.byt[1]);
+  // Serial.print("d");
+  // Serial.write((uint8_t *)m->message, m->length.length);
+
   // Serial.flush();
   // Serial.println("heylot");
   // Serial.print(m->length);
@@ -73,42 +120,45 @@ void recv_message(struct Message *m) {
   // Serial.read((uint8_t *)&m->length, 2);
   // Serial.read(&m->message, m->length);
   
-  send_message(m);
-  Serial.print("hello");
+  // send_message(m);
+  // Serial.print("hello");
 }
 
 void setup() {
   // put your setup code h¨ere, to run once:
   Serial.begin(9600);
+  strip.begin();
+  strip.show(); // Initialize all pixels to 'off'
+  strip.setBrightness(8);
 
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
-  // int incomingByte = 0;
+  int incomingByte = 0;
   // if (Serial.available() > 0)
   // {
-  //   // read the incoming byte:
+  // //   // read the incoming byte:
   //   incomingByte = Serial.read();
 
-  //   // say what you got:
-  //   // Serial.print("I received: ");
+  // //   // say what you got:
+  // //   // Serial.print("I received: ");
   //   Serial.write(incomingByte);
   // }
   // Serial.print("test");
-  struct Message m;
 
-  m.type = 0x6d;
-  m.length = 11;
-  m.message = "abcdefghijk";
   // send_message(m);
-  free(&m);
+  // free(&m);
   struct Message *me;
+  me = malloc(sizeof(struct Message));
 
-  if (Serial.available() >= 3) {
+
+  if (Serial.available() > 0) {
     recv_message(me);
+    send_message(me);
 
   }
 
+  // Serial.print("f");
   delay(1000);
 }
--- python/passman.py
+++ python/passman.py
@@ -1,5 +1,7 @@
 import serial
 import serial.tools.list_ports
+import struct
+import time
 
 BAUD_RATE = 9600
 
@@ -9,7 +11,7 @@ def init():
     port = None
     for device in devices:
         # print(device.name, device.location, device.device, device.description)
-        if device.description == "Feather 32u4":
+        if device.description == "Feather 32u4" or device.description == "Adafruit Feather":
             print("Found Passman device on", device.device)
             port = device.device
 
@@ -30,17 +32,52 @@ def init():
         stopbits=serial.STOPBITS_TWO,
         bytesize=serial.SEVENBITS
     )
-
+    
     if ser.is_open:
         return ser
     print("Serial failed to init")
     exit(1)
 
+
+def get_message(ser):
+    # print("hello")
+    # type = ser.read(1)
+    # for i in range(3):
+    #     print(ser.read(1))
+    by = ser.read(3)
+    # print(by)
+    type, length = struct.unpack("<ch", by)
+    if type.decode("ascii") == "m":
+        # length = int.from_bytes(ser.read(2), byteorder='big')
+        print(type, length)
+        print(ser.read(length))
+    else:
+        print("gross:", str(type))
+
+def send_message(ser, type, message):
+    # print(message.encode())
+    # s = bytes(message, "ascii")
+    # print(s)
+    struct_fmt = "!ch{}s".format(len(message))
+    string = struct.pack(struct_fmt, type.encode(), len(message), message.encode())
+    # time.sleep(1)
+    ser.write(string)
+    print(string)
+
+
 def main():
+    
     ser = init()
+    # send_message(ser, "m", "I can type any message")
+    # while True:
+    #     print(ser.read(1))
+    #     # print("aa")
+
 
     while True:
-        print(ser.read())
+        send_message(ser, "m", "test")
+        get_message(ser)
+        # print(ser.read())
 
 
 
